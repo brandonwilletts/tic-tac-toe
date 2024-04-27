acb79bb4e57da699bec324da9c7d89a274600bd0
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-04-27 14:30:20 -0700

    Create console game
    Basic console gameplay works

---
+++ javascript.js
@@ -0,0 +1,104 @@
+function gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i][j] = "0";
+        }
+    };
+
+    const renderBoard = function() {
+        return console.table(board);
+    }
+
+    const addMarker = function(x, y, marker) {
+        board[y][x] = marker;
+    }
+
+    const getBoard = function() {
+        return board;
+    }
+
+    return { renderBoard, addMarker, getBoard }
+}
+
+function gameController() {
+    const board = gameboard();
+    const players = [
+        {
+            name: prompt("Player 1: Please enter your name", "Player 1"),
+            marker: 1
+        },
+        {
+            name: prompt("Player 2: Please enter your name", "Player 2"),
+            marker: 2
+        }
+    ];
+    let activePlayer = players[0];
+
+    const playGame = function() {
+        console.log(`${players[0].name} versus ${players[1].name}! Let's go!`);
+        board.renderBoard();
+        playRound();
+    }
+
+    const switchActivePlayer = function() {
+        activePlayer === players[0] ? activePlayer = players[1] : activePlayer = players[0];
+    }
+    
+    const checkWinner = function(player) {
+        const boardArray = board.getBoard();
+        let result = false;
+
+        let diagonalToCheckOne = [];
+        let diagonalToCheckTwo = [];
+        
+        for (let i = 0; i < boardArray.length; i++) {
+
+            let rowToCheck = boardArray[i];
+            rowToCheck.every(item => item === player.marker) ? result = true : null;
+
+            let columnToCheck = [];
+            for (let j = 0; j < boardArray.length; j++) {
+                columnToCheck[j] = boardArray[j][i];
+            }
+            columnToCheck.every(item => item === player.marker) ? result = true : null;
+
+            diagonalToCheckOne[i] = boardArray[i][i];
+            for (let k = (boardArray[i].length - 1); k >= 0; k--){
+                diagonalToCheckTwo[i] = boardArray[i][k];
+            }
+        }
+
+        diagonalToCheckOne.every(item => item === player.marker) ? result = true : null;
+        diagonalToCheckTwo.every(item => item === player.marker) ? result = true : null;
+
+        return result
+    }
+
+    const playRound = function() {
+        console.log(`${activePlayer.name}'s turn`)
+
+        let playerSelectionX = prompt("Enter X-coordinate (0-2)");
+        let playerSelectionY = prompt("Enter Y-coordinate (0-2)");
+        
+        board.addMarker(playerSelectionX, playerSelectionY, activePlayer.marker);
+        board.renderBoard();
+        
+        if (checkWinner(activePlayer)) {
+            console.log(`${activePlayer.name} wins!`)
+        } else {
+            console.log("No winner yet");
+            switchActivePlayer();
+            playRound();
+        }
+    }
+    
+    playGame();
+
+}
+
+gameController();
